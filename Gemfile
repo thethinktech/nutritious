038e0af64896854abd54daf8d25b8e061358b850
munamata <munam.ata@tftus.com>
2016-01-09 22:13:55 +0530

    Minor changes in assets

--- Gemfile
+++ Gemfile
@@ -12,8 +12,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'font-awesome-rails', '~> 4.5.0'
 gem 'sass-rails', '~> 5.0'
-gem 'font-awesome-rails', '~> 4.5'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Nutritious
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.enabled = true
+    config.assets.compress = true
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
