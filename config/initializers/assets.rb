e3315a921a3c77a016d36c51e5f687d6fb7e6693
Md Munam <mdmunam@infotachus.com>
2016-01-11 15:32:12 +0530

    Changes to precompile font files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w[*.woff *.css *.js *.png *.jpg *.jpeg *.gif]
+Rails.application.config.assets.precompile += %w[*.woff *.css *.js *.png *.jpg *.jpeg *.gif *.svg *.eot *.woff *.ttf ]
