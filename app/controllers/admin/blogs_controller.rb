ccb6d2f3bd16baf85bb5ee26ff941f0868f9ed42
neel <neel@ubuntu.(none)>
2016-02-07 21:24:15 -0800

    add blog controller

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -27,6 +27,23 @@ class Admin::BlogsController < Admin::AdminController
 	    end
     end
 
+      def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_blogs_path, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+    end
+
+      def update
+       if @blog.update(blog_params)
+      redirect_to admin_blog_path, notice: "Blog Updated!"
+       else
+      flash[:alert] = "Blog could not be updated"
+      render "edit"
+       end
+       end
 
   	def blog_params
     	params.require(:blog).permit(:title, :decsription, :image, :user_id, :category_id)
